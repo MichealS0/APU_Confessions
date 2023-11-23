2ddc3f8938ee2530d5b2f896175d523686ab9607
Theron Aiolos <awaabkheiri7177@gmail.com>
2023-11-23 18:09:48 +0800

    Fetch

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+  const [confession, setConfession] = useState(null);
+
+  useEffect(() => {
+    fetch('localhost:8800/ConfessionsMain')
+    .then((response) => response.json())
+    .then((data) => {
+      setConfession(data[0].confession);
+      console.log(data)
+    })
+    .catch((error) => console.log(error));
+  })
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{confession}</h1>
     </div>
   );
 }
